c7013de11d72a7d403fe73396553280840fb67f8
totto2727 <kaihatu.totto2727@gmail.com>
2022-11-21 02:19:19 +0900

    Improve:RabbitMQに対応

--- twitter-list-observer/src/main.rs
+++ twitter-list-observer/src/main.rs
@@ -4,8 +4,17 @@ mod twitter;
 use std::error::Error;
 
 use futures::prelude::*;
-use rust_lib::{env::load_env, redis::client::get_redis_client, re_export::redis::Commands};
-use tracing::{error, info, warn};
+use rust_lib::rabbitmq::channel;
+use rust_lib::re_export::deadpool_lapin::lapin::options::{
+    BasicPublishOptions, QueueDeclareOptions,
+};
+use rust_lib::re_export::deadpool_lapin::lapin::types::FieldTable;
+use rust_lib::re_export::deadpool_lapin::lapin::BasicProperties;
+use rust_lib::re_export::redis::JsonAsyncCommands;
+use rust_lib::re_export::{serde, serde_json};
+use rust_lib::redis::client::get_redis_client;
+use rust_lib::{env::load_env, portal::Message};
+use tracing::{error, info};
 use twitter::{convert_message, get_api_app_ctx, query_stream, Rule};
 
 #[tokio::main]
@@ -14,50 +23,58 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     load_env()?;
 
-    // let twitter = get_api_app_ctx()?;
+    let twitter = get_api_app_ctx()?;
 
-    let redis = get_redis_client()?;
-    let mut redis_con = redis.get_connection()?;
+    let rules = [
+        Rule::new("La priere", "from:Lapriere_info"),
+    ];
 
-    redis_con.set("my_key", 42)?;
-    redis_con.get("my_key")?;
+    Rule::query_initialize_rules(&twitter, &rules).await?;
 
-    return Ok(())
-    // let rules = [
-    //     Rule::new("La priere", "from:Lapriere_info"),
-    //     Rule::new("Cat", "猫"),
-    // ];
-    //
-    // Rule::query_initialize_rules(&twitter, &rules).await?;
-    //
-    // loop {
-    //     let stream = match query_stream(&twitter).await {
-    //         Ok(ok) => ok,
-    //         Err(err) => {
-    //             error!("fail to query stream:{:?}", err);
-    //             tokio::time::sleep(tokio::time::Duration::from_secs(960)).await;
-    //             continue;
-    //         }
-    //     };
-    //
-    //     info!("stream start");
-    //
-    //     futures::pin_mut!(stream);
-    //     while let Some(item) = stream.next().await {
-    //         let message = match convert_message(item) {
-    //             Ok(ok) => ok,
-    //             Err(_) => break,
-    //         };
-    //
-    //         if let Ok(serialized) = serde_json::to_string(&message) {
-    //             info!(serialized);
-    //         } else {
-    //             warn!("fail to map message");
-    //         }
-    //     }
-    //
-    //     info!(r#"stream stop"#);
-    //     tokio::time::sleep(tokio::time::Duration::from_secs(960)).await;
-    //     info!("stream restarting");
-    // }
+    let channel = channel().await?;
+
+    channel
+        .queue_declare(
+            "portal_message",
+            QueueDeclareOptions::default(),
+            FieldTable::default(),
+        )
+        .await?;
+
+    loop {
+        let stream = match query_stream(&twitter).await {
+            Ok(ok) => ok,
+            Err(err) => {
+                error!("fail to query stream:{:?}", err);
+                tokio::time::sleep(tokio::time::Duration::from_secs(960)).await;
+                continue;
+            }
+        };
+
+        info!("stream start");
+
+        futures::pin_mut!(stream);
+        while let Some(item) = stream.next().await {
+            let message = match convert_message(item) {
+                Ok(ok) => ok,
+                Err(_) => break,
+            };
+
+            let json = serde_json::to_string(&message)?;
+            channel
+                .basic_publish(
+                    "",
+                    "portal_message",
+                    BasicPublishOptions::default(),
+                    Vec::from(json),
+                    BasicProperties::default(),
+                )
+                .await?;
+            info!("{:?}", message);
+        }
+
+        info!("stream stop");
+        tokio::time::sleep(tokio::time::Duration::from_secs(960)).await;
+        info!("stream restarting");
+    }
 }
